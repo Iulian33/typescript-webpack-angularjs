f48fe4006db65a238ba0ebfe0ecc4482b2bc10b3
steevehook <steevehook@gmail.com>
2017-11-16 02:22:04 +0200

    add todo.js tasks

---
+++ todo.js
@@ -0,0 +1,11 @@
+// 1. Add boilerplate component code for each topic in playground topics
+// 2. Create highlighter component
+// 3. Create logger component
+// 4. Add examples and highlights for each individual example in playground topics
+// 5. Add an express server for current client project
+// 6. Deploy to heroku + update README.md with heroku deployed app
+// 7. Update readme with image
+// 8. Update readme with project structure explanation
+// 9. Rename github repository and description (something like webpack-typescript-angularjs)
+// 10. Add server side example (create a node express app with couple of routes and controllers)
+// 11. Make a keynote presentation
