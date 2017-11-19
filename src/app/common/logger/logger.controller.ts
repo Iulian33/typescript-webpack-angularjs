5f31513e3957c79d4554e9682ef253aaff327cdc
steevehook <steevehook@gmail.com>
2017-11-20 01:23:48 +0200

    initialize showdown feature

--- src/app/common/logger/logger.controller.ts
+++ src/app/common/logger/logger.controller.ts
@@ -1,6 +1,25 @@
-import {IComponentController} from 'angular';
+import {IComponentController, ITranscludeFunction} from 'angular';
+import showdown = require('showdown');
+import {Converter} from 'showdown';
+import $ = require('jquery');
 
 class LoggerController implements IComponentController {
+    public static $inject: string[] = ['$transclude'];
+
+    private converter: Converter;
+
+    constructor(private $transclude: ITranscludeFunction) {
+        $transclude((clone, scope) => {
+            console.log(clone.text());
+            console.log(scope);
+            // $scope.text = clone.text().toUpperCase();
+        });
+        // this.converter = new showdown.Converter();
+        // $(document).ready(() => {
+        //     const text = $('.logger__text').text();
+        //     $('.logger__text').html(this.converter.makeHtml('\`Hello\`'));
+        // });
+    }
 }
 
 export default LoggerController;
